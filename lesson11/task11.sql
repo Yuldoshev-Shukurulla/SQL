ddbe0644f8023dd38319053bea628a7669c22ce6
Yuldoshev-Shukurulla <yuldashevshukurulla2801@gmail.com>
2026-08-21 15:50:00 +0500

    task11

--- lesson11/task11.sql
+++ lesson11/task11.sql
@@ -17,25 +17,52 @@
 -- Example: Alice (HR) moves to IT, Bob (IT) moves to Sales, Charlie (Sales) moves to HR.
 -- 3. Insert the updated records into #EmployeeTransfers. 
 -- 4. Retrieve all records from #EmployeeTransfers.
+
 CREATE TABLE Employees
 (
-    EmployeeID INT IDENTITY(1, 1) PRIMARY KEY,
+    EmployeeID INT PRIMARY KEY,
     Name VARCHAR(50),
     Department VARCHAR(50),
     Salary INT
 );
-INSERT INTO Employees(Name, Department, Salary)
-VALUES  ('Alice', 'HR', 5000),
-        ('Bob', 'IT', 7000),
-        ('Charlie', 'Sales', 6000),
-        ('David', 'HR', 5500),
-        ('Emma', 'IT', 7200);
+GO
 
+INSERT INTO Employees
+VALUES  (1, 'Alice', 'HR', 5000),
+        (2, 'Bob', 'IT', 7000),
+        (3, 'Charlie', 'Sales', 6000),
+        (4, 'David', 'HR', 5500),
+        (5, 'Emma', 'IT', 7200);
+
+DROP TABLE IF EXISTS #EmployeeTransfers;
+CREATE TABLE #EmployeeTransfers
+(
+    EmployeeID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Department VARCHAR(50),
+    Salary INT
+);
+
+INSERT INTO #EmployeeTransfers (EmployeeID, Name, Department, Salary)
+SELECT
+    EmployeeID,
+    Name,
+    CASE Department
+        WHEN 'HR' THEN 'IT'
+        WHEN 'IT' THEN 'Sales'
+        WHEN 'Sales' THEN 'HR'
+        ELSE Department 
+    END AS Department,
+    Salary
+FROM Employees;
+
+SELECT * FROM #EmployeeTransfers ORDER BY EmployeeID;
 
 -- ---
 
 -- Puzzle 2: The Missing Orders
--- An e-commerce company tracks orders in two separate systems, but some orders are missing in one of them. You need to find the missing records.
+-- An e-commerce company tracks orders in two separate systems, but some orders are missing in one of them. 
+-- You need to find the missing records.
 
 -- Given Tables:
 -- Table 1: Orders_DB1 (Main System)
@@ -53,6 +80,46 @@ VALUES  ('Alice', 'HR', 5000),
 -- Task:
 -- 1. Declare a table variable @MissingOrders with the same structure as Orders_DB1. 2. Insert all orders that exist in Orders_DB1 
 -- but not in Orders_DB2 into @MissingOrders. 3. Retrieve the missing orders.
+
+CREATE TABLE Orders_DB1
+(
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100),
+    Product VARCHAR(50),
+    Quantity INT
+);
+
+CREATE TABLE Orders_DB2
+(
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100),
+    Product VARCHAR(50),
+    Quantity INT
+);
+INSERT INTO Orders_DB1
+VALUES
+(101, 'Alice', 'Laptop', 1),
+(102, 'Bob', 'Phone', 2),
+(103, 'Charlie', 'Tablet', 1),
+(104, 'David', 'Monitor', 1);
+
+INSERT INTO Orders_DB2
+VALUES
+(101, 'Alice', 'Laptop', 1),
+(103, 'Charlie', 'Tablet', 1);
+
+DECLARE @MissingOrders TABLE(
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100),
+    Product VARCHAR(50),
+    Quantity INT
+);
+INSERT INTO @MissingOrders (OrderID, CustomerName, Product, Quantity)
+SELECT db1.* FROM Orders_DB1 db1
+LEFT JOIN Orders_DB2 db2
+ON db1.OrderID=db2.OrderID
+WHERE db2.OrderID IS NULL;
+SELECT * FROM @MissingOrders
 
 -- ---
 
@@ -79,4 +146,44 @@ VALUES  ('Alice', 'HR', 5000),
 -- Department, AvgHoursDepartment (AVG hours worked per department).
 -- 2. Retrieve all records from vw_MonthlyWorkSummary.
 
--- Materials: https://drive.google.com/drive/folders/1Lx0y31m2yU_iEAgbCuzhtWnC14rqCBxJ?usp=sharing
+CREATE TABLE WorkLog (
+    EmployeeID INT,
+    EmployeeName VARCHAR(50),
+    Department VARCHAR(50),
+    WorkDate DATE,
+    HoursWorked INT
+);
+
+INSERT INTO WorkLog (EmployeeID, EmployeeName, Department, WorkDate, HoursWorked) VALUES
+(1, 'Alice',   'HR',    '2024-03-01', 8),
+(2, 'Bob',     'IT',    '2024-03-01', 9),
+(3, 'Charlie', 'Sales', '2024-03-02', 7),
+(1, 'Alice',   'HR',    '2024-03-03', 6),
+(2, 'Bob',     'IT',    '2024-03-03', 8),
+(3, 'Charlie', 'Sales', '2024-03-04', 9);
+GO
+
+SELECT * FROM WorkLog;
+GO
+
+CREATE VIEW vw_MonthlyWorkSummary AS
+SELECT
+    EmployeeID,
+    EmployeeName,
+    Department,
+    TotalHoursWorked,
+    SUM(TotalHoursWorked) OVER (PARTITION BY Department) AS TotalHoursDepartment,
+    AVG(TotalHoursWorked) OVER (PARTITION BY Department) AS AvgHoursDepartment
+FROM (
+    SELECT 
+        EmployeeID,
+        EmployeeName,
+        Department,
+        SUM(HoursWorked) AS TotalHoursWorked
+    FROM WorkLog
+    GROUP BY EmployeeID, EmployeeName, Department
+) AS T;
+GO
+
+SELECT * FROM vw_MonthlyWorkSummary
+ORDER BY Department, EmployeeID;
